e7b622e51bf82ad5328080839dd280bb1ca3eca3
skyline75489 <skyline75489@outlook.com>
2015-03-23 20:05:37 +0800

    pep8

--- dg.py
+++ dg.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 
-import os
 import sys
 import pathlib
 import argparse
 import json
+
 
 def read_json(file_path):
     with open(file_path) as f:
@@ -14,7 +14,8 @@ def read_json(file_path):
             print("Error: JSON file is not valid.")
             sys.exit(1)
         return r
-    
+
+
 def generate_dir_with_json(json, dir_path):
     current_path = pathlib.Path(dir_path)
     for i in json:
@@ -26,12 +27,13 @@ def generate_dir_with_json(json, dir_path):
                 output_path = current_path.joinpath(key)
                 output_path.mkdir()
                 generate_dir_with_json(i[key], output_path)
-    
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description = "A dir generator"
+        description="A dir generator"
     )
-    parser.add_argument('-d', '--dir', type=str, 
+    parser.add_argument('-d', '--dir', type=str,
                         help='set target dir path. defaults to ./',
                         default='./')
     parser.add_argument('-f', '--file', type=str,
@@ -42,6 +44,6 @@ def main():
     dir_path = args.dir
     json = read_json(file_path)
     generate_dir_with_json(json, dir_path)
-    
+
 if __name__ == '__main__':
     main()
